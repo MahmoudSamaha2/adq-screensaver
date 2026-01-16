459139736db85c32c350f46fddb1323a42b6fb79
MahmoudSamaha2 <mahmoud.samaha@dotment.com>
2026-01-16 21:30:52 +0200

    Update video handling in MainForm
    Extract embedded video to a temporary file and play it.

--- MainForm.cs
+++ MainForm.cs
@@ -10,7 +10,9 @@ namespace ScreensaverPlayer
         private LibVLC _libVLC;
         private MediaPlayer _mediaPlayer;
         private VideoView _videoView;
-        private string videoPath = "adq screensaver.mp4";
+    private string videoResourceName = "ScreensaverPlayer.adq screensaver.mp4";
+    private string tempVideoPath;
+
 
         public MainForm(string[] args)
         {
@@ -18,6 +20,7 @@ namespace ScreensaverPlayer
             this.WindowState = FormWindowState.Maximized;
             this.TopMost = true;
             this.Load += MainForm_Load;
+            this.FormClosed += MainForm_FormClosed;
             this.KeyDown += (s, e) => this.Close();
             this.MouseMove += (s, e) => this.Close();
             this.MouseClick += (s, e) => this.Close();
@@ -25,6 +28,14 @@ namespace ScreensaverPlayer
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // Extract embedded video to temp file
+            tempVideoPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Guid.NewGuid().ToString() + ".mp4");
+            using (var stream = typeof(MainForm).Assembly.GetManifestResourceStream(videoResourceName))
+            using (var file = System.IO.File.OpenWrite(tempVideoPath))
+            {
+                stream.CopyTo(file);
+            }
+
             Core.Initialize();
             _libVLC = new LibVLC();
             _mediaPlayer = new MediaPlayer(_libVLC);
@@ -35,7 +46,7 @@ namespace ScreensaverPlayer
             };
             this.Controls.Add(_videoView);
 
-            var media = new Media(_libVLC, videoPath, FromType.FromPath);
+            var media = new Media(_libVLC, tempVideoPath, FromType.FromPath);
             _mediaPlayer.Play(media);
             _mediaPlayer.EndReached += (s, ev) =>
             {
@@ -43,6 +54,17 @@ namespace ScreensaverPlayer
                 _mediaPlayer.Stop();
                 _mediaPlayer.Play(media);
             };
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Clean up temp file
+            try
+            {
+                if (!string.IsNullOrEmpty(tempVideoPath) && System.IO.File.Exists(tempVideoPath))
+                    System.IO.File.Delete(tempVideoPath);
+            }
+            catch { }
         }
 
         protected override void Dispose(bool disposing)
